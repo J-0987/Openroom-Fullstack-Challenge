fb07c0e00e98b18c1a2397a05c8c59a639590125
Jasmine J <146461479+J-0987@users.noreply.github.com>
2024-11-17 15:48:00 -0500

    fixed insomnia bug: streamlined  syntax mismatch v1 and v2 and added get and del definitions in crud.py

--- app/crud.py
+++ app/crud.py
@@ -1,10 +1,22 @@
+from fastapi import Depends
 from sqlalchemy.orm import Session
 from .models import DriverLicenseApplication
 from .schemas import DriverLicenseCreate
+from .main import app 
 
 def create_driver_license(db: Session, license_data: DriverLicenseCreate):
     db_license = DriverLicenseApplication(**license_data.dict())
     db.add(db_license)
     db.commit()
     db.refresh(db_license)
+    return db_license
+
+
+def get_driver_licenses(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(DriverLicenseApplication).offset(skip).limit(limit).all()
+
+def delete_driver_license(db: Session, license_id: int):
+    db_license = db.query(DriverLicenseApplication).filter(DriverLicenseApplication.id == license_id).first()
+    db.delete(db_license)
+    db.commit()
     return db_license
