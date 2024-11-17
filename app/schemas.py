3ba667a162ab5cf19f19900b7131bafbbf279ce6
Jasmine J <146461479+J-0987@users.noreply.github.com>
2024-11-17 16:46:26 -0500

    further chnges to meet V2 rules in schemas

--- app/schemas.py
+++ app/schemas.py
@@ -1,16 +1,17 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 from datetime import date
+from typing import Annotated
 
 class DriverLicenseBase(BaseModel):
-    last_name: str
-    first_name: str
-    middle_name: str = None
-    license_number: str = None
+    last_name: str = Field(min_length=2, max_length=50)
+    first_name: str = Field(min_length=2, max_length=50)
+    middle_name: str | None = None 
+    license_number: str
     date_of_birth: date
     sex: str
     height_cm: int
     residential_address: str
-    mailing_address: str = None
+    mailing_address: str 
     province: str
     postal_code: str
 
@@ -20,5 +21,4 @@ class DriverLicenseCreate(DriverLicenseBase):
 class DriverLicense(DriverLicenseBase):
     id: int
 
-class Config:
-        from_attributes = True
+model_config = ConfigDict(from_attributes=True)
