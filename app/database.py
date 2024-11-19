332d955b4f1a46552ff759ff66335430f544fe1f
Jasmine J <146461479+J-0987@users.noreply.github.com>
2024-11-19 14:33:02 -0500

    conflict merge trial - changed coe on database.py line 13

--- app/database.py
+++ app/database.py
@@ -10,6 +10,6 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmakerFAST(autocommit=False, autoflush=False, bind=engine)
 
 print("database.py loaded")
